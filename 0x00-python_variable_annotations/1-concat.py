8a061a021a57f5c99421d7d4b20be8d14426e92d
craizytech <craizytech@gmail.com>
2024-10-12 21:21:06 +0300

    ype-annotated function concat that takes a string str1 and a string str2 as arguments and returns a concatenated string

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""This module contains a method that takes two string and concatenates them"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    This function takes two strings and returns a concatenated string
+
+    Args:
+        str1 (str): this is the first string
+        atr2 (str): this is the second string
+
+    Returns:
+        (str): the concatenated version of the two strings.
+    """
+    return str1 + str2
