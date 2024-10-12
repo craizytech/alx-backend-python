e12d43ad0b5d418ad16b657c09f80054087d87dd
craizytech <craizytech@gmail.com>
2024-10-12 20:54:23 +0300

    documented the python code to add docstrings

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,3 +1,15 @@
 #!/usr/bin/env python3
+"""This module takes 2 numbers and adds them together."""
+
 def add(a: float, b: float) -> float:
+    """
+    This function takes two floats and sums them up together
+
+    Args:
+        a (int): The first argument
+        b (float): the second argument
+    
+    Returns:
+        (float) the result of adding the 2 numbers together
+    """
     return a + b
