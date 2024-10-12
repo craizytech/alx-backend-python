156a6f2706a5cbf8d110ed083d1dfc324c986d32
craizytech <craizytech@gmail.com>
2024-10-12 20:56:29 +0300

    documented the python function

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """This module takes 2 numbers and adds them together."""
 
+
 def add(a: float, b: float) -> float:
     """
     This function takes two floats and sums them up together
@@ -8,7 +9,7 @@ def add(a: float, b: float) -> float:
     Args:
         a (int): The first argument
         b (float): the second argument
-    
+
     Returns:
         (float) the result of adding the 2 numbers together
     """
