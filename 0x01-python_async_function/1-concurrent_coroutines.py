4709074a8e0855a53a53f3465428a97bcb9fbd39
craizytech <craizytech@gmail.com>
2024-10-15 02:30:07 +0300

    fixed some type annotations and also ensured that pycodestyle isn't complaining

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-This module covers: Let's execute multiple coroutines at the same time with async
+This module covers: Let's execute multiple coroutines at the same
+time with async
 """
 
 
@@ -9,13 +10,15 @@ import asyncio
 from typing import Callable, Awaitable, List
 
 
-wait_random: Callable[[int], Awaitable[float]] = __import__('0-basic_async_syntax').wait_random
+wait_random: Callable[[int], Awaitable[float]] = \
+    __import__('0-basic_async_syntax').wait_random
 
-async def wait_n(n: int, max_delay:int) -> List[float]:
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
     delays: List = []
 
     for i in range(n):
         delay = await wait_random(max_delay)
         delays.append(delay)
-    
+
     return delays
